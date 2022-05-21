5660cb7d9f8e537e013baceba305cbbd81734015
Merzaad <Merzaad@gmail.com>
2022-05-21 13:18:48 +0430

    test query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { useQuery } from 'react-query'
 
-const App = () => (
-
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <div className="Card">Hello</div>
-    </header>
-  </div>
-)
+const App = () => {
+  const { isLoading, data } = useQuery('test', () =>
+    fetch('https://api.blockchair.com/dogecoin/stats').then((res) => res.json())
+  )
+  return (
+    <div className="App">
+      <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="Card">
+            doge_average_transaction_fee_24h: {isLoading && 'Loading'} {data && data.data.average_transaction_fee_24h}
+          </div>
+      </header>
+    </div>
+  )
+}
 
 export default App
