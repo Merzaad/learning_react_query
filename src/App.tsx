3c0155c1babb78a304553e614d3753bd38ec26d9
Merzaad <Merzaad@gmail.com>
2022-05-25 11:04:30 +0430

    fetch with parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import * as React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { useQuery } from 'react-query'
 import Rq from './components/rq'
 
 const App = () => {
-  const fetchData = async () => {
-    const response = await fetch('https://api.blockchair.com/dogecoin/stats')
+  const [coin, setCoin] = React.useState('dogecoin')
+  const fetchData = async (coin: string) => {
+    const response = await fetch(`https://api.blockchair.com/${coin}/stats`)
     return response.json()
   }
-
-  const test = useQuery('test', fetchData)
-
+  const test = useQuery(['coin', coin], () => fetchData(coin))
+  const switchCoinHandler = () => {
+    if (coin === 'bitcoin') setCoin('dogecoin')
+    if (coin === 'dogecoin') setCoin('bitcoin')
+  }
+  const buttonTxt = coin === 'bitcoin' ? 'dogecoin' : 'bitcoin'
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button style={{
+          padding: 20,
+          fontSize: 20,
+        }} onClick={switchCoinHandler}>{buttonTxt}</button>
         <Rq data={test} />
       </header>
     </div>
--- src/components/rq.tsx
+++ src/components/rq.tsx
@@ -9,15 +9,20 @@ const Rq = ({ data }: test) => {
   return (
     <div
       style={{
-        minWidth: '40%',
-        height: 100,
         background: 'white',
         color: 'black',
+        borderRadius: 15,
+        overflowWrap: 'anywhere',
+        padding: 50,
+        textAlign: 'left',
+        margin: 50,
+        boxShadow: '0px 0px 10px black'
       }}
     >
       {x.isLoading && 'is loading'}
       {x.error && x.error.message}
-      {x.data && x.data.data.average_transaction_fee_24h}
+      server : {x.data && x.data.context.servers} <br />
+      {x.data && JSON.stringify(x.data)}
     </div>
   )
 }
