68c87ff80bbe1301f859eac36e45cba4aec90f09
Merzaad <Merzaad@gmail.com>
2022-05-31 19:36:11 +0430

    WebOTP API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query'
 import CoinCard from './components/card'
 
 const App = () => {
-  const fetchData = async (coin: string) => {
+  const fetchData = async (coin) => {
     const response = await fetch(`https://api.blockchair.com/${coin}/stats`)
     if (response.status === 200) return response.json()
     return new Promise((resolve) => resolve({ data: { transactions: 'status !== 200' } }))
@@ -14,6 +14,25 @@ const App = () => {
     const query = useQuery(['coin', coin], () => fetchData(coin))
     return <CoinCard coinResponse={query} key={coin} coin={coin} />
   })
+  const submit = () => {
+    alert('submited')
+  }
+  React.useEffect(() => {
+    if ('OTPCredential' in window) {
+      const ac = new AbortController()
+      navigator.credentials
+        .get({
+          otp: { transport: ['sms'] },
+          signal: ac.signal,
+        })
+        .then((otp) => {
+          submit(otp.code)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  })
   return (
     <div
       style={{
@@ -23,6 +42,12 @@ const App = () => {
       }}
     >
       {cards}
+      <form>
+        <input />
+        <button onClick={submit} >
+          submit
+        </button>
+      </form>
     </div>
   )
 }
