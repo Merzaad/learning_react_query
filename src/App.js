a8280a364553f3edc0170ba062a82bb36933ec98
Mehrzad Hosseini <Merzaad@gmail.com>
2022-06-01 17:20:09 +0430

    asd

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const submit = (otp) => {
     alert(otp)
   }
-  const test = 'testasdasdasdasdasdase'
+  const test = 'testasdasdasdasdasdasdasdase'
   React.useEffect(() => {
     if ('OTPCredential' in window) {
       window.addEventListener('DOMContentLoaded', (e) => {
