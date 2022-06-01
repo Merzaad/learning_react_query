1b45b454b29e4691bae56abbc2d2879f455cc68e
Mehrzad Hosseini <Merzaad@gmail.com>
2022-06-01 15:57:30 +0430

    Test reabase

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const submit = (otp: 'string') => {
     alert(otp)
   }
-  const test = 'testtttttt rebase'
+  const test = 'testtttttt asdasd'
   React.useEffect(() => {
     if ('OTPCredential' in window) {
       window.addEventListener('DOMContentLoaded', (e) => {
