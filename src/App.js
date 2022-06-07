c2a18b43d641bd6a4070b308d0af783df3f6555e
Merzaad <Merzaad@gmail.com>
2022-06-07 17:12:18 +0430

    rebase conflict

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   const submit = (otp) => {
     alert(otp)
   }
-  const test = 'test_vscode_rebase'
+  const test = 'rebase_test'
   React.useEffect(() => {
     if ('OTPCredential' in window) {
       window.addEventListener('DOMContentLoaded', (e) => {
