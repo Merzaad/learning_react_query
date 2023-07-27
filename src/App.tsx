d0e54afba451579af8d526a70d828137146db8b7
merzaad <merzaad@gmail.com>
2023-07-27 07:56:51 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center h-full border border-teal-500 rounded-lg'>
+        React Query
+      </div>
+    </>
+  )
+}
+
+export default App
